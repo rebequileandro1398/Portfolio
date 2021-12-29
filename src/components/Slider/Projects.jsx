3f3a7bb45f3b719020f04571d859705d3f619d17
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-12-29 20:04:09 -0300

    poketfit

--- src/components/Slider/Projects.jsx
+++ src/components/Slider/Projects.jsx
@@ -2,25 +2,30 @@
 import project3 from '../../assets/weatherapp.jpg'
 export const Items = [
     {
+        src: "https://www.youtube.com/embed/gXtg8IngScg",
+        alt:'proyecto 4',
+        desc:'PoketFit',
+        web: "https://snack.expo.dev/@lheitt/pocketfit-app",
+        id: "1"
+    },
+    {
         src: "https://www.youtube.com/embed/rXOETtXZOAc",
         alt:'proyecto 3',
         desc:'WeatherApp',
-        web: 'https://vercel-weather-app.vercel.app/'
+        web: 'https://vercel-weather-app.vercel.app/',
+        id: "2"
     },
     {
         src: "https://www.youtube.com/embed/WDtV_DV-DzQ",
         alt:'proyecto 1',
         desc:'PI-Countries',
-        web:'https://pi-countries-beta.vercel.app/'
+        web:'https://pi-countries-beta.vercel.app/',
+        id: "3"
     },
     {
         src: "https://www.youtube.com/embed/cFri3TfW-ho",
         alt:'proyecto 2',
-        desc:'Pex Gallery'
-    },
-    {
-        src: project3,
-        alt:'proyecto 4',
-        desc:'PoketFit'
+        desc:'Pex Gallery',
+        id: "4"
     }
 ]
